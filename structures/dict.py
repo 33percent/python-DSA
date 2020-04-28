cb0ecfcb268cd37c418fd4659b28ca52e6057bbb
sandeep <sandeep@uniqolabel.com>
2020-04-28 23:07:55 +0530

    dictionary revision

--- structures/dict.py
+++ structures/dict.py
@@ -1,7 +1,37 @@
-d = {
-    'alice':182731872,
-    'bob':'a,hsdklas'
+#create dic
+my_dict = {
+    'name':'pawan',
+    'age':56
 }
 
-d['sandeep'] = '6i172389'
-print(d)
+#update dict
+my_dict['age'] = 57
+my_dict['last_name'] = 'Kalyan'
+
+
+#fetch value in dict
+print(my_dict['age'])
+print(my_dict.get('first_name'))
+
+#removing an element
+my_dict.pop('age')
+print(my_dict)
+my_dict['age'] = 57
+
+
+print(my_dict.items())
+print(my_dict.keys())
+print(my_dict.values())
+
+#Python Dictionary Comprehension
+squares = { x:x for x in range(6)}
+print(squares)
+
+cubes = { x:x*x*x for x in range(11) if x%3 == 0}
+print(cubes)
+
+print('marriage' in my_dict)
+
+
+for i in my_dict:
+    print('{} ---> {}'.format(i, my_dict[i]))
