cda4dcccc5bd9432117e9312d62031b42c96a0a3
sandeep <sandeep0814@gmail.com>
2020-04-29 22:21:34 +0530

    lists revision

--- structures/lists.py
+++ structures/lists.py
@@ -1,5 +1,29 @@
-[1,2,3]
-a = ['asd','asd','1ih2']
-b = []
-b.append(1.12)
-print(b)
+my_list = [1,2,3,4]
+
+print(my_list[1])
+
+print(my_list[-1])
+
+
+my_list[0] = 2
+print(my_list)
+
+my_list.append(7)
+print(my_list)
+
+my_list.extend([90])
+
+my_list.insert(0,9)
+print(my_list)
+print(my_list[0:2])
+
+del my_list[0]
+del my_list[0:1]
+print(my_list)
+
+
+pow2 = [2 ** x for x in range(10)]
+print(pow2)
+
+odd = [x for x in range(20) if x % 2 == 1]
+print(odd)
